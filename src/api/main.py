faaef178b61bb0a7e8587b9eaa01c5ec2f2bd030
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-09 02:03:41 +0300

    Add question endpoint

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, FastAPI
 from src.api.dependencies import get_vqa_port
 from src.domain.models.input.captioning_input import CaptioningInput
+from src.domain.models.input.question_input import QuestionInput
 from src.domain.models.output.response import Response
 from src.domain.ports.vqa_port import VqaPort
 from src.api.schemas import HealthResponse
@@ -22,4 +23,17 @@ def predict(
     vqa_port: VqaPort = Depends(get_vqa_port),
 ) -> Response:
     response = vqa_port.process_captioning(captioning_input)
+    return response
+
+@app.post(
+    "/vqa/question",
+    response_model=Response,
+    summary="Answer question about uploaded image.",
+    description="Accepts an image file, performs QA, and returns anwer"
+)
+def answer(
+    question_input: QuestionInput,
+    vqa_port: VqaPort = Depends(get_vqa_port),
+) -> Response:
+    response = vqa_port.process_question(question_input)
     return response
