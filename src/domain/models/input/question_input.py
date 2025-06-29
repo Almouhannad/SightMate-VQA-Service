562969c88f5bbfeffb30a86e14bd49ca5118ccb6
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-29 18:27:25 +0300

    Modify domain

--- src/domain/models/input/question_input.py
+++ src/domain/models/input/question_input.py
@@ -1,10 +1,12 @@
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 from pydantic import BaseModel
 
+from src.domain.models.input.history_item import HistoryItem
 from src.domain.models.input.image_input import ImageInput
 
 
 class QuestionInput(BaseModel):
     image: ImageInput
     question: str
+    history: Optional[List[HistoryItem]] = None
     options: Optional[Dict[str, object]] = None
